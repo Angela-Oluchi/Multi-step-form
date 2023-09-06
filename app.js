c39c85c23faa47e2a707d2003baf5e634f4099e1
Angela Ria <angelaabugu01@gmail.com>
2023-09-06 15:12:53 +0100

    thanks-added

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 let nextBtn = document.getElementById("next-btn")
 let genSteps = document.getElementById("all-step")
+let check = addDetails.getElementById("check")
+let addDetails = document.querySelector(".add-details")
 
 if (nextBtn) {
     nextBtn.addEventListener('click', () => {
         genSteps.classList.add('active')
+    })
+}
+if (check) {
+    check.addEventListener('click', () => {
+        addDetails.classList.add('active')
     })
 }
